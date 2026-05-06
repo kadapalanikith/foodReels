b4299225429cab076e7479ebe442651c0e89d96f
nikithreddy <kadapalanikith@gmail.com>
2026-05-06 23:34:37 +0530

    added logout functionality

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -61,4 +61,10 @@ async function loginUser(req, res) {
     });
 }
 
+async function logoutUser(req, res) {
+    res.clearCookie('token');
+    res.status(200).json({ message: 'Logout successful' });
+}
+
+
 module.exports = {registerUser, loginUser};
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -5,7 +5,8 @@ const router = express.Router();
 
 
 
-router.post('/user/register', registerUser)
-routes.post('/user/login', loginUser)
+router.post('/user/register', authController.registerUser)
+router.post('/user/login', authController.loginUser)
+router.get('/user/logout', authController.logoutUser)
 
 module.exports = router;
